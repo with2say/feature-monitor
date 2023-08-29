6447752cf2b36a7f5d537dfcf6d9e49e773efe38
with2say <with2say@gmail.com>
2023-08-29 17:23:31 +0900

    Add influxDB writer

---
+++ monitor/core/db_writer.py
@@ -0,0 +1,56 @@
+from typing import Dict, Any
+
+import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+INFLUXDB_TOKEN = "Hhw8zbuj0BukaLT-0lAVgB2gfKUdT0a_FDBWd7AXuZUJeFQE7lg1HMAnaOi4lLHzXyZBhlY3rVKWFKkh3fEoVA=="
+ORG = "a13"
+BUCKET = 'test'
+URL = "http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(url=URL, token=INFLUXDB_TOKEN, org=ORG)
+
+
+def write(measurement_name: str,
+          tags: Dict[str, Any],
+          fields: Dict[str, Any],
+          bucket: str = BUCKET,
+          org: str = ORG,
+          verbose: bool = False,
+          ) -> None:
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    point = Point(measurement_name)
+
+    for tag_key, tag_value in tags.items():
+        point = point.tag(tag_key, tag_value)
+
+    for field_key, field_value in fields.items():
+        point = point.field(field_key, field_value)
+
+    if verbose:
+        print(point)
+
+    write_api.write(bucket=bucket, org=org, record=point)
+
+
+def read(measurement_name: str,
+         bucket: str = BUCKET,
+         org: str = ORG,
+         ) -> None:
+    query_api = client.query_api()
+    query = """from(bucket: "{}")
+         |> range(start: -10m)
+         |> filter(fn: (r) => r._measurement == "{}")
+         """.format(bucket, measurement_name)
+    print(query)
+    tables = query_api.query(query, org=org)
+
+    for table in tables:
+        for record in table.records:
+            print(record)
+
+
+if __name__ == "__main__":
+    read(measurement_name='node_monitoring')
+
